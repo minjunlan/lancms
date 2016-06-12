5eb1aaa5544c7f827c44e4ca8505319834655776
unknown <>
2016-06-12 11:21:27 +0800

    db

--- core/db.go
+++ core/db.go
@@ -9,20 +9,47 @@ import (
 
 // DB 定义数据库类，用来操作数据库
 type DB struct {
-	sql.DB
+	db                    *sql.DB
 	dbname, dbuser, dbpwd string
+	data                  []map[string]interface{}
 }
 
-func (d *DB) Conn(dbname, dbuser, dbpwd string) (*sql.DB, error) {
-	link := dbuser + ":" + dbpwd + "@/" + dbname
-	db, err := sql.Open("mysql", link)
+// Result 返回的结果
+type Result struct {
+}
+
+//连接数据库
+func (d *DB) Conn(dbname, dbuser, dbpwd string) (*DB, error) {
+	db, err := sql.Open("mysql", dbuser+":"+dbpwd+"@/"+dbname)
 	if err != nil {
 		return nil, err
 	}
-
-	return db, nil
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	d.db = db
+	return d, nil
 }
 
-func Init() {
+//获取结果
+func (d *DB) Result() *DB {
+	fmt.Println("resuilt")
+	return d
+}
+
+func (d *DB) Insert(sql string, args ...interface{}) *Result {
+
+}
+func (d *DB) Select(sql string, args ...interface{}) *Result {
+
+}
+func (d *DB) Delete(sql string, args ...interface{}) *Result {
+
+}
+func (d *DB) Update(sql string, args ...interface{}) *Result {
+
+}
+func init() {
 	fmt.Print("chu shi hau")
 }
--- core/db_test.go
+++ core/db_test.go
@@ -1,33 +1,17 @@
 package lan
 
-import (
-	"fmt"
-	"testing"
-)
+import "testing"
 
 func TestConn(t *testing.T) {
 	var db = new(DB)
 
 	conn, err := db.Conn("test", "root", "root")
 
-	row, err2 := conn.Query("select * from test")
-	if err2 != nil {
-		fmt.Printf("%v\n", err2)
-	}
-	var x1 = make([]interface{}, 4)
-
-	for row.Next() {
-		// cols, _ := row.Columns()
-
-		err := row.Scan(&x1[0], &x1[1], &x1[2], &x1[3])
-		if err != nil {
-			fmt.Printf("cols:%#v\n", err)
-		}
-
-	}
-	fmt.Printf("cols:%s\n", x1[3])
 	if err != nil {
-		t.Errorf("连接对象时出错:%s\n", err)
+		t.Errorf("conn 函数出错：%#v\n", err)
 	}
 
+	if conn == nil {
+		t.Errorf("conn 为空：%#v\n", conn)
+	}
 }
